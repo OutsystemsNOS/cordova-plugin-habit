fec45e73418b23658e47b2f9834b72752857f6ef
miguelrosanos <79104255+miguelrosanos@users.noreply.github.com>
2021-09-14 16:28:50 +0100

    Update HabitPlugin.java

--- src/Android/HabitPlugin.java
+++ src/Android/HabitPlugin.java
@@ -1,16 +1,4 @@
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.graphics.Color;
-import android.graphics.drawable.Drawable;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-
 import org.json.JSONObject;
-
-import java.util.HashMap;
-import java.util.Map;
 
 import io.habit.android.devicehealth.DeviceHealth;
 import io.habit.android.devicehealth.DeviceInfoCallback;
