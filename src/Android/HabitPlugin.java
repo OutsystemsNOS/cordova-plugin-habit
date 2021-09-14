9e0d22ae5eb189b294d65bd6c47941db3547dc4e
miguelrosanos <79104255+miguelrosanos@users.noreply.github.com>
2021-09-14 15:53:42 +0100

    Update HabitPlugin.java

--- src/Android/HabitPlugin.java
+++ src/Android/HabitPlugin.java
@@ -1,37 +1,10 @@
-package com.habit.devicetest;
+package org.apache.cordova.plugin;
 
+import org.apache.cordova.api.CordovaPlugin;
+import org.apache.cordova.api.PluginResult;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.apache.cordova.CordovaPlugin;
-import org.apache.cordova.CallbackContext;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.graphics.Color;
-import android.graphics.drawable.Drawable;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-
-import org.json.JSONObject;
-
-import java.util.HashMap;
-import java.util.Map;
-
-import io.habit.android.devicehealth.DeviceHealth;
-import io.habit.android.devicehealth.DeviceInfoCallback;
-import io.habit.android.devicehealth.TestCallback;
-import io.habit.android.devicehealth.global.Globals;
-import io.habit.android.devicehealth.global.ScreenType;
-
-import io.habit.android.devicehealth.model.BorderType;
-import io.habit.android.devicehealth.model.ButtonStyle;
-import io.habit.android.devicehealth.model.CustomizableScreen;
-import io.habit.android.devicehealth.model.Customization;
-import io.habit.android.devicehealth.util.Utils;
-import io.habit.android.devicehealth.lang.ScreenCustomizationKeys;
 
 public class HabitPlugin extends CordovaPlugin {
 
