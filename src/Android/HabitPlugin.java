c1fab1ee0970c4800c45afb4f540edec626cb429
miguelrosanos <79104255+miguelrosanos@users.noreply.github.com>
2021-09-14 16:04:08 +0100

    Update HabitPlugin.java

--- src/Android/HabitPlugin.java
+++ src/Android/HabitPlugin.java
@@ -1,4 +1,32 @@
 package org.apache.cordova.plugin;
+package com.habit.devicetest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import io.habit.android.devicehealth.DeviceHealth;
+import io.habit.android.devicehealth.DeviceInfoCallback;
+import io.habit.android.devicehealth.TestCallback;
+import io.habit.android.devicehealth.global.Globals;
+import io.habit.android.devicehealth.global.ScreenType;
+
+import io.habit.android.devicehealth.model.BorderType;
+import io.habit.android.devicehealth.model.ButtonStyle;
+import io.habit.android.devicehealth.model.CustomizableScreen;
+import io.habit.android.devicehealth.model.Customization;
+import io.habit.android.devicehealth.util.Utils;
+import io.habit.android.devicehealth.lang.ScreenCustomizationKeys;
 
 import org.apache.cordova.api.CordovaPlugin;
 import org.apache.cordova.api.PluginResult;
