19cb382c680512544d8bc169980b7ceb99fad0ba
miguelrosanos <79104255+miguelrosanos@users.noreply.github.com>
2021-09-14 13:30:29 +0100

    Update HabitPlugin.java

--- src/Android/HabitPlugin.java
+++ src/Android/HabitPlugin.java
@@ -1,1 +1,34 @@
 package org.apache.cordova.habit;
+
+import io.habit.android.devicehealth.DeviceHealth;
+import io.habit.android.devicehealth.DeviceInfoCallback;
+import io.habit.android.devicehealth.TestCallback;
+import io.habit.android.devicehealth.global.Globals;
+import io.habit.android.devicehealth.global.ScreenType;
+
+import io.habit.android.devicehealth.model.BorderType;
+import io.habit.android.devicehealth.model.ButtonStyle;
+import io.habit.android.devicehealth.model.CustomizableScreen;
+import io.habit.android.devicehealth.model.Customization;
+import io.habit.android.devicehealth.util.Utils;
+import io.habit.android.devicehealth.lang.ScreenCustomizationKeys;
+
+public class HabitPlugin extends CordovaPlugin {
+
+  @Override
+  public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+    if (action.equals("getDeviceInfo")) {      
+        DeviceHealth.getDeviceInfo(this, this, args.getString(0), args.getString(1), new DeviceInfoCallback() {
+          @Override
+          public void onResponse(JSONObject obj) {
+            this.getDeviceInfo(obj);
+          }
+        });
+      return true;      
+    } else if (action.equals("performTests")) {
+      return true;
+    } 
+  }
+    return false;
+  }
+}
