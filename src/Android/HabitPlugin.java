c6ec94a392089af992d5135cc9cd10d36dd7c97c
miguelrosanos <79104255+miguelrosanos@users.noreply.github.com>
2021-09-16 13:33:59 +0100

    Update HabitPlugin.java

--- src/Android/HabitPlugin.java
+++ src/Android/HabitPlugin.java
@@ -15,6 +15,7 @@ import io.habit.android.devicehealth.lang.ScreenCustomizationKeys;
 
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.PluginResult;
+import org.apache.cordova.CallbackContext;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
