544c4f10b1639f72a116a41e0f34c6b116539475
miguelrosanos <79104255+miguelrosanos@users.noreply.github.com>
2021-09-16 14:59:23 +0100

    Update habit.js

--- www/habit.js
+++ www/habit.js
@@ -2,6 +2,6 @@ var exec = require('cordova/exec');
 
 var PLUGIN_NAME = 'HabitPlugin';
 
-exports.getDeviceInfo = function (number, success, error) {
-  exec(success, error, PLUGIN_NAME, "getDeviceInfo", [number]);
+exports.getDeviceInfo = function (number, imei, success, error) {
+  exec(success, error, PLUGIN_NAME, "getDeviceInfo", [number, imei]);
 };
