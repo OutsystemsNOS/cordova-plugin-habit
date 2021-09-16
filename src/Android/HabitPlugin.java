9cb7fc772ead514f5dee74502e69b8374f2a3655
miguelrosanos <79104255+miguelrosanos@users.noreply.github.com>
2021-09-16 13:51:28 +0100

    Update HabitPlugin.java

--- src/Android/HabitPlugin.java
+++ src/Android/HabitPlugin.java
@@ -24,6 +24,17 @@ public class HabitPlugin extends CordovaPlugin {
 
   @Override
   public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-    return false;	
-  }
+    if (action.equals("getDeviceInfo ")) {
+            DeviceHealth.getDeviceInfo(this, this, "<-serial number->", "<-device IMEI->", new DeviceInfoCallback() {
+                @Override
+                public void onResponse(JSONObject obj) {
+
+                // obj == null : Canceled
+                // obj != null : Success
+
+                }
+            });
+            return true;
+        } 
+    }
   }
