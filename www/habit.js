70b7d23353b7bd66db953035cb91f42bb623493d
miguelrosanos <79104255+miguelrosanos@users.noreply.github.com>
2021-09-17 15:07:27 +0100

    Update habit.js

--- www/habit.js
+++ www/habit.js
@@ -5,4 +5,5 @@ var PLUGIN_NAME = 'HabitPlugin';
 exports.getDeviceInfo = function (number, imei, success, error) {
   alert("number: " + number + " imei: " + imei);
   exec(success, error, PLUGIN_NAME, "getDeviceInfo", [number, imei]);
+  alert(exec done);
 };
