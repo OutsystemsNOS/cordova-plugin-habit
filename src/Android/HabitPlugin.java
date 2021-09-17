d8d5721a372c5e3af2e9b10b14cc1d28bc8d664f
miguelrosanos <79104255+miguelrosanos@users.noreply.github.com>
2021-09-17 09:54:58 +0100

    Update HabitPlugin.java

--- src/Android/HabitPlugin.java
+++ src/Android/HabitPlugin.java
@@ -28,9 +28,11 @@ public class HabitPlugin extends CordovaPlugin {
   @Override
   public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
     if (action.equals("getDeviceInfo")) {
-            this.getDeviceInfo(callbackContext, args.getString(0), args.getString(1));
+            //this.getDeviceInfo(callbackContext, args.getString(0), args.getString(1));
+            callbackContext.success(args.getString(1))
             return true;
         } 
+    callbackContext.error(args.getString(1))
     return false;
     }
    
