11eea31d7a18dc1e9e31379ae0dae45fd7782bab
Miguel Rosa <79104255+miguelrosanos@users.noreply.github.com>
2021-09-21 14:46:57 +0100

    Update habit.js

--- www/habit.js
+++ www/habit.js
@@ -9,15 +9,3 @@ exports.getDeviceInfo = function (serialnumber, imei, success, error) {
 exports.performTests = function (appid, apikey, serialnumber, imei, tests, success, error) {
   exec(success, error, PLUGIN_NAME, "performTests", [appid, apikey, serialnumber, imei, tests]);
 };
-
-exports.hideStartScreen = function (hide, success, error) {
-  exec(success, error, PLUGIN_NAME, "hideStartScreen", [hide]);
-};
-
-exports.setLanguage = function (language, imei, success, error) {
-  exec(success, error, PLUGIN_NAME, "setLanguage", [language]);
-};
-
-exports.setThemeColor = function (red, blue, green, success, error) {
-  exec(success, error, PLUGIN_NAME, "setThemeColor", [red, blue, green]);
-};
