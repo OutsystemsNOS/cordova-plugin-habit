ba5c66632fce5597e64ed2fcda905d6ba118200b
Miguel Rosa <79104255+miguelrosanos@users.noreply.github.com>
2021-09-22 16:25:27 +0100

    Update HabitPlugin.java

--- src/android/HabitPlugin.java
+++ src/android/HabitPlugin.java
@@ -138,20 +138,3 @@ public class HabitPlugin extends CordovaPlugin {
     	});
   	}
   }
-public class Customization {
-	
-	String ScreenTitle;
-	String ScreenDescription;
-	String ScreenBackgroundColor;
-	String ScreenTextAccentColor;
-	String ScreenTestColor;
-	String ButtonStyleBackgroundColor;
-	String ButtonStyleForegroundColor;
-	String ButtonStyleBorderType;
-	String StyleSkipTestButtonColor;
-	String StyleProgressBarBackgroundColor;
-	String ProgressBarSelectedColor;
-	String CustomNavigationBarBackgroundColor;
-	String CustomNavigationBarTextColor;
-	String CustomNavigationBarButtonsTextColor;
-}
