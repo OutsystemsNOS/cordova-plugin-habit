ce40936c48859999c9288a3f96ebebd4ee5c06e7
miguelrosanos <miguel.rosa@moxie.pt>
2021-09-22 16:36:12 +0100

    Create MyCustomization.java

---
+++ src/android/MyCustomization.java
@@ -0,0 +1,17 @@
+public class MyCustomization {
+	
+	String ScreenTitle;
+	String ScreenDescription;
+	String ScreenBackgroundColor;
+	String ScreenTextAccentColor;
+	String ScreenTestColor;
+	String ButtonStyleBackgroundColor;
+	String ButtonStyleForegroundColor;
+	String ButtonStyleBorderType;
+	String StyleSkipTestButtonColor;
+	String StyleProgressBarBackgroundColor;
+	String ProgressBarSelectedColor;
+	String CustomNavigationBarBackgroundColor;
+	String CustomNavigationBarTextColor;
+	String CustomNavigationBarButtonsTextColor;
+}
