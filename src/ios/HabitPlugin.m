dcf058fbb2deccca246911cca5ee20184e1ae9a3
Miguel Rosa <79104255+MiguelRosaDev@users.noreply.github.com>
2021-09-24 11:31:10 +0100

    Update HabitPlugin.m

--- src/ios/HabitPlugin.m
+++ src/ios/HabitPlugin.m
@@ -7,9 +7,18 @@
 @synthesize performTestsCallback;
 
 - (void)getDeviceInfo:(CDVInvokedUrlCommand *)command {
-
+        NSString* serialnumber = [[command.arguments objectAtIndex:0]];
+        NSString* imei = [[command.arguments objectAtIndex:1]];
 }
 
 - (void)performTests:(CDVInvokedUrlCommand *)command {
-
+        NSString* appid = [[command.arguments objectAtIndex:0]];
+        NSString* apikey = [[command.arguments objectAtIndex:1]];
+        NSString* serialnumber = [[command.arguments objectAtIndex:2]];
+        NSString* imei = [[command.arguments objectAtIndex:3]];
+        NSString* testsToPerform = [[command.arguments objectAtIndex:4]];
+        NSString* language = [[command.arguments objectAtIndex:5]];
+        NSString* themecolor = [[command.arguments objectAtIndex:6]];
+        BOOL hidesstartcreen = [[command.arguments objectAtIndex:7] boolValue];
+        NSString* screencustomization = [[command.arguments objectAtIndex:8]];
 }
