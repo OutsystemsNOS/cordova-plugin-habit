e2ce4b2492b14e889318710b8e94519139823bc4
Miguel Rosa <79104255+MiguelRosaDev@users.noreply.github.com>
2021-09-24 11:10:24 +0100

    Update HabitPlugin.h

--- src/ios/HabitPlugin.h
+++ src/ios/HabitPlugin.h
@@ -3,9 +3,6 @@
 @interface HabitPlugin : CDVPlugin {
 }
 
-
-@property (assign) NSString* callback;
-
 // The hooks for our plugin commands
 - (void)getDeviceInfo:(CDVInvokedUrlCommand *)command;
 - (void)performTests:(CDVInvokedUrlCommand *)command;
