4681af945e6878959c170beeed247b297155aabb
Miguel Rosa <79104255+MiguelRosaDev@users.noreply.github.com>
2021-10-06 15:09:01 +0100

    Update HabitPlugin.swift

--- src/ios/HabitPlugin.swift
+++ src/ios/HabitPlugin.swift
@@ -10,14 +10,28 @@ import DeviceHealth
     }
 
     public func getDeviceInfo(_ command: CDVInvokedUrlCommand) {
-
-        let pluginResult = CDVPluginResult(status:CDVCommandStatus_OK, messageAsString:@"Ok")
-        self.commandDelegate?.send(pluginResult, callbackId: command.callbackId)
+        let serialnumber = [[command.arguments objectAtIndex:0]];
+        let imeinumber = [[command.arguments objectAtIndex:1]];
+        CDVPluginResult* pluginResult = nil;
+        
+        @try {
+        DeviceHealthSDK.shared.getDeviceInfo(imei: imeinumber, serialNumber: serialnumber) { (result) in               
+                if (result != nil) {
+                        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:result];
+                } else {
+                        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:"Some error getting device info"];
+                }                                                                                                                                                                    
+                [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+        }
+        }@catch (NSException* exception) {
+              pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:[exception reason]];  
+              [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+        }
     }
 
     public func performTests(_ command: CDVInvokedUrlCommand) {
 
-        let pluginResult = CDVPluginResult(status:CDVCommandStatus_OK, messageAsString:@"Ok")
+        let pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:"Ok"];
         self.commandDelegate?.send(pluginResult, callbackId: command.callbackId)
     }
 
