6ed3a46586c6efb83314f5ecaadca842521e08c5
Miguel Rosa <79104255+MiguelRosaDev@users.noreply.github.com>
2021-10-07 15:48:21 +0100

    Update HabitPlugin.swift

--- src/ios/HabitPlugin.swift
+++ src/ios/HabitPlugin.swift
@@ -14,10 +14,31 @@ import DeviceHealth
         let serialnumber = command.arguments[0] as? String ?? ""
         let imeinumber = command.arguments[1] as? String ?? ""
         
-        DeviceHealthSDK.shared.getDeviceInfo(imei: imeinumber, serialNumber: serialnumber) { (result) in               
-                let result = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: result)
+        DeviceHealthSDK.shared.getDeviceInfo(imei: imeinumber, serialNumber: serialnumber) { (result) in                             
+                let stringObject = self.JSONStringify(result!, prettyPrinted: true)
+                let status = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: stringObject)
                 self.commandDelegate.send(result, callbackId: command.callbackId)
         }
+    }
+    
+    func JSONStringify(_ value: [String:Any],prettyPrinted:Bool = false) -> String
+    {
+        let options = prettyPrinted ? JSONSerialization.WritingOptions.prettyPrinted : JSONSerialization.WritingOptions(rawValue: 0)
+
+        if JSONSerialization.isValidJSONObject(value) {
+
+            do{
+                let data = try JSONSerialization.data(withJSONObject: value, options: options)
+                if let string = NSString(data: data, encoding: String.Encoding.utf8.rawValue) {
+                    return string as String
+                }
+            } catch {
+
+//                dLog(message: "error")
+            }
+        }
+        return ""
+
     }
 
 }
