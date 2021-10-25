3cc4facccf6863b6ba1d7303f4f06f838a6bb965
Miguel Rosa <79104255+MiguelRosaDev@users.noreply.github.com>
2021-10-25 12:24:42 +0100

    Update HabitPlugin.m

--- src/ios/HabitPlugin.m
+++ src/ios/HabitPlugin.m
@@ -5,8 +5,8 @@
 @implementation HabitPlugin
 
 - (void)getDeviceInfo:(CDVInvokedUrlCommand *)command {
-        NSString* serialnumber = [[command.arguments objectAtIndex:0]];
-        NSString* imeinumber = [[command.arguments objectAtIndex:1]];
+        NSString* serialnumber = [command.arguments objectAtIndex:0];
+        NSString* imeinumber = [command.arguments objectAtIndex:1];
         CDVPluginResult* pluginResult = nil;
         
         @try{   
@@ -25,15 +25,15 @@
 }
 
 - (void)performTests:(CDVInvokedUrlCommand *)command {
-        NSString* appid = [[command.arguments objectAtIndex:0]];
-        NSString* apikey = [[command.arguments objectAtIndex:1]];
-        NSString* serialnumber = [[command.arguments objectAtIndex:2]];
-        NSString* imei = [[command.arguments objectAtIndex:3]];
-        NSString* testsToPerform = [[command.arguments objectAtIndex:4]];
-        NSString* language = [[command.arguments objectAtIndex:5]];
-        NSString* themecolor = [[command.arguments objectAtIndex:6]];
+        NSString* appid = [command.arguments objectAtIndex:0];
+        NSString* apikey = [command.arguments objectAtIndex:1];
+        NSString* serialnumber = [command.arguments objectAtIndex:2];
+        NSString* imei = [command.arguments objectAtIndex:3];
+        NSString* testsToPerform = [[command.arguments objectAtIndex:4];
+        NSString* language = [command.arguments objectAtIndex:5];
+        NSString* themecolor = [command.arguments objectAtIndex:6];
         BOOL hidesstartcreen = [[command.arguments objectAtIndex:7] boolValue];
-        NSString* screencustomization = [[command.arguments objectAtIndex:8]];
+        NSString* screencustomization = [command.arguments objectAtIndex:8];
         
         CDVPluginResult* pluginResult = nil;
         
