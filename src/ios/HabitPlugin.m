2134743323b555f2b0db2c5581f9fb2548c3cb23
Miguel Rosa <79104255+MiguelRosaDev@users.noreply.github.com>
2021-10-25 13:58:49 +0100

    Update HabitPlugin.m

--- src/ios/HabitPlugin.m
+++ src/ios/HabitPlugin.m
@@ -21,17 +21,44 @@
         NSString* appid = [command.arguments objectAtIndex:0];
         NSString* apikey = [command.arguments objectAtIndex:1];
         NSString* serialnumber = [command.arguments objectAtIndex:2];
-        NSString* imei = [command.arguments objectAtIndex:3];
+        NSString* imeinumber = [command.arguments objectAtIndex:3];
         NSString* testsToPerform = [command.arguments objectAtIndex:4];
         NSString* language = [command.arguments objectAtIndex:5];
         NSString* themecolor = [command.arguments objectAtIndex:6];
         BOOL hidesstartcreen = [[command.arguments objectAtIndex:7] boolValue];
         NSString* screencustomization = [command.arguments objectAtIndex:8];
         
-        __block CDVPluginResult* pluginResult = nil;
+        __block CDVPluginResult* pluginResult = nil;  
         
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"Ok"];
-        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+        let selectedTests = [ScreenType.buttons_v2, ScreenType.charging_v2, ScreenType.multi_touch_v2, ScreenType.device_front_video_v2 ]
+                
+        let customization = Customization()
+
+        var customStartCopy = [String: String]()
+        customStartCopy[ScreenCustomizationKeys.start_screen.Copy.title] = "Your custom title"
+        customStartCopy[ScreenCustomizationKeys.start_screen.Copy.description] = "Your custom description"
+
+        //var images = [String: UIImage]()
+        //images[ScreenCustomizationKeys.start_screen.Elements.image] = UIImage(named: "your_custom_image")
+
+        let screen = CustomizableScreen(screenType : ScreenType.start_screen)
+        screen.backgroundColor = UIColor.white
+        screen.textAccentColor = UIColor.blue
+        screen.textColor = UIColor.black
+        //screen.images = images
+        screen.copyStrings = customStartCopy
+
+        customization.customScreens.append(screen)
+        
+        @try{
+        [[DeviceHealthSDK shared] performTestsWithAppID:appid apiKey:apikey testsToPerform:selectedTests imei:imeinumber serialNumber:serialnumber customization:customization completion:^(NSDictionary<NSString *,id> * result) {
+                pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:result];
+                [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+        }];
+        }@catch (NSException* exception) {
+              CDVPluginResult* pluginResultErr = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:[exception reason]];  
+              [self.commandDelegate sendPluginResult:pluginResultErr callbackId:command.callbackId];
+        }
 }
 
 @end
