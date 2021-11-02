c87096d19f6b5603ad6c742bbb7bf6ab88515ce4
Miguel Rosa <79104255+MiguelRosaDev@users.noreply.github.com>
2021-11-02 17:27:00 +0000

    Update HabitPlugin.h

--- src/ios/HabitPlugin.h
+++ src/ios/HabitPlugin.h
@@ -4,10 +4,31 @@
 @interface HabitPlugin : CDVPlugin {
 }
 
-@property (nonatomic, readwrite) NSString *ScreenTitle;
-
 // The hooks for our plugin commands
 - (void)getDeviceInfo:(CDVInvokedUrlCommand *)command;
 - (void)performTests:(CDVInvokedUrlCommand *)command;
+
+@end
+
+@interface MyCustomization : NSObject {
+      NSString *ScreenTitle;
+       /*
+       @property (nonatomic, strong) NSString *ScreenDescription;
+       @property (nonatomic, strong) NSString *ScreenBackgroundColor;
+       @property (nonatomic, strong) NSString *ScreenTextAccentColor;
+       @property (nonatomic, strong) NSString *ScreenTestColor;
+       @property (nonatomic, strong) NSString *ButtonStyleBackgroundColor;
+       @property (nonatomic, strong) NSString *ButtonStyleForegroundColor;
+       @property (nonatomic, strong) NSString *ButtonStyleBorderType;
+       @property (nonatomic, strong) NSString *StyleSkipTestButtonColor;
+       @property (nonatomic, strong) NSString *StyleProgressBarBackgroundColor;
+       @property (nonatomic, strong) NSString *ProgressBarSelectedColor;
+       @property (nonatomic, strong) NSString *CustomNavigationBarBackgroundColor;
+       @property (nonatomic, strong) NSString *CustomNavigationBarTextColor;
+       @property (nonatomic, strong) NSString *CustomNavigationBarButtonsTextColor;
+       */
+}
+
+@property (nonatomic, readwrite) NSString *ScreenTitle;
 
 @end
