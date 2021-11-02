3af4711b552d410ad017e8bf6035a70c9ea92987
Miguel Rosa <79104255+MiguelRosaDev@users.noreply.github.com>
2021-11-02 17:11:04 +0000

    Update HabitPlugin.h

--- src/ios/HabitPlugin.h
+++ src/ios/HabitPlugin.h
@@ -4,6 +4,8 @@
 @interface HabitPlugin : CDVPlugin {
 }
 
+@property (nonatomic, readwrite) NSString *ScreenTitle;
+
 // The hooks for our plugin commands
 - (void)getDeviceInfo:(CDVInvokedUrlCommand *)command;
 - (void)performTests:(CDVInvokedUrlCommand *)command;
