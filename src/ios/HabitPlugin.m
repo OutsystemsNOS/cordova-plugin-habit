85de0b4d3ae30139e41b49cc1ba2a8b9056c0cef
Miguel Rosa <79104255+MiguelRosaDev@users.noreply.github.com>
2021-11-02 15:25:40 +0000

    Update HabitPlugin.m

--- src/ios/HabitPlugin.m
+++ src/ios/HabitPlugin.m
@@ -56,10 +56,9 @@
 
         if(! error) {
                 NSMutableArray *array = [[NSMutableArray alloc] init];
-
-        MyCustomization *oMyCustomization = [[MyCustomization alloc] init];
                 
-        for (NSString *dictionaryKey in object) {           
+        for (NSString *dictionaryKey in object) {  
+            MyCustomization *oMyCustomization = [[MyCustomization alloc] init];
             oMyCustomization.ScreenTitle = [[object valueForKey:dictionaryKey] objectForKey:@"ScreenTitle"];
             oMyCustomization.ScreenDescription = [[object valueForKey:dictionaryKey] objectForKey:@"ScreenDescription"];
             oMyCustomization.ScreenBackgroundColor = [[object valueForKey:dictionaryKey] objectForKey:@"ScreenBackgroundColor"];
