ff4c2c69d6fd54af10d5c85ad8324d70da832a64
Miguel Rosa <79104255+MiguelRosaDev@users.noreply.github.com>
2021-11-02 17:26:08 +0000

    Update HabitPlugin.m

--- src/ios/HabitPlugin.m
+++ src/ios/HabitPlugin.m
@@ -3,27 +3,6 @@
 
 @import DeviceHealth;
 
-@interface MyCustomization : NSObject {
-      NSString *ScreenTitle;
-       /*
-       @property (nonatomic, strong) NSString *ScreenDescription;
-       @property (nonatomic, strong) NSString *ScreenBackgroundColor;
-       @property (nonatomic, strong) NSString *ScreenTextAccentColor;
-       @property (nonatomic, strong) NSString *ScreenTestColor;
-       @property (nonatomic, strong) NSString *ButtonStyleBackgroundColor;
-       @property (nonatomic, strong) NSString *ButtonStyleForegroundColor;
-       @property (nonatomic, strong) NSString *ButtonStyleBorderType;
-       @property (nonatomic, strong) NSString *StyleSkipTestButtonColor;
-       @property (nonatomic, strong) NSString *StyleProgressBarBackgroundColor;
-       @property (nonatomic, strong) NSString *ProgressBarSelectedColor;
-       @property (nonatomic, strong) NSString *CustomNavigationBarBackgroundColor;
-       @property (nonatomic, strong) NSString *CustomNavigationBarTextColor;
-       @property (nonatomic, strong) NSString *CustomNavigationBarButtonsTextColor;
-       */
-}
-
-@end
-
 @implementation HabitPlugin
 
 @synthesize ScreenTitle;
