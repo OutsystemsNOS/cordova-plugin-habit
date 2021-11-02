2814bba2892b4b410a817f216510ea1df94d01d6
miguelrosanos <miguel.rosa@moxie.pt>
2021-11-02 10:18:31 +0000

    new version

---
+++ src/ios/Frameworks/Swift5.3 Support/DeviceHealth.xcframework/ios-arm64_x86_64-simulator/DeviceHealth.framework/Headers/HealthLibrary.h
@@ -0,0 +1,18 @@
+//
+//  HealthLibrary.h
+//  HealthLibrary
+//
+//  Created by Ana Figueira on 15/01/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HealthLibrary.
+FOUNDATION_EXPORT double HealthLibraryVersionNumber;
+
+//! Project version string for HealthLibrary.
+FOUNDATION_EXPORT const unsigned char HealthLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HealthLibrary/PublicHeader.h>
+
+
