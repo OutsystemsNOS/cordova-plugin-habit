c8c5cedcd206d953ca6cd476aa9a8c9c8079bbfe
Miguel Rosa <79104255+MiguelRosaDev@users.noreply.github.com>
2021-11-02 14:26:22 +0000

    Update HabitPlugin.h

--- src/ios/HabitPlugin.h
+++ src/ios/HabitPlugin.h
@@ -1,24 +1,5 @@
 #import <Cordova/CDV.h>
 
-#import <Foundation/Foundation.h>
-
-@interface MyCustomization : NSObject
-@property (nonatomic, strong) NSString *ScreenTitle;
-@property (nonatomic, strong) NSString *ScreenDescription;
-@property (nonatomic, strong) NSString *ScreenBackgroundColor;
-@property (nonatomic, strong) NSString *ScreenTextAccentColor;
-@property (nonatomic, strong) NSString *ScreenTestColor;
-@property (nonatomic, strong) NSString *ButtonStyleBackgroundColor;
-@property (nonatomic, strong) NSString *ButtonStyleForegroundColor;
-@property (nonatomic, strong) NSString *ButtonStyleBorderType;
-@property (nonatomic, strong) NSString *StyleSkipTestButtonColor;
-@property (nonatomic, strong) NSString *StyleProgressBarBackgroundColor;
-@property (nonatomic, strong) NSString *ProgressBarSelectedColor;
-@property (nonatomic, strong) NSString *CustomNavigationBarBackgroundColor;
-@property (nonatomic, strong) NSString *CustomNavigationBarTextColor;
-@property (nonatomic, strong) NSString *CustomNavigationBarButtonsTextColor;
-
-@end
 
 @interface HabitPlugin : CDVPlugin {
 }
