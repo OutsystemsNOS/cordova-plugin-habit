4eaaa3cdd929812ff4b69af1a48cf02f59cc430f
Miguel Rosa <79104255+MiguelRosaDev@users.noreply.github.com>
2021-11-12 09:57:26 +0000

    Update HabitPlugin.m

--- src/ios/HabitPlugin.m
+++ src/ios/HabitPlugin.m
@@ -121,14 +121,12 @@
                         [selectedTests addObject:ScreenType.device_front_video_v2];
                 }
                 
-                [self.commandDelegate runInBackground:^{
-                        [[DeviceHealthSDK shared] performTestsWithAppID:appid apiKey:apikey testsToPerform:selectedTests imei:imeinumber serialNumber:serialnumber customization:customization completion:^(NSDictionary<NSString *,id> * result) {
-                                NSError * err;
-                                NSData * jsonData = [NSJSONSerialization dataWithJSONObject:result options:0 error:&err]; 
-                                NSString * myString = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
-                                CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:myString];
-                                [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-                        }];
+                [[DeviceHealthSDK shared] performTestsWithAppID:appid apiKey:apikey testsToPerform:selectedTests imei:imeinumber serialNumber:serialnumber customization:customization completion:^(NSDictionary<NSString *,id> * result) {
+                        NSError * err;
+                        NSData * jsonData = [NSJSONSerialization dataWithJSONObject:result options:0 error:&err]; 
+                        NSString * myString = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
+                        CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:myString];
+                        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
                 }];
                 
         }@catch (NSException* exception) {
