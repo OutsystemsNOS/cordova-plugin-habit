6efc20f621916c3c6a6a564f8702e09b24e16d1e
Miguel Rosa <79104255+MiguelRosaDev@users.noreply.github.com>
2021-11-24 12:07:24 +0000

    Update HabitPlugin.java

--- src/android/HabitPlugin.java
+++ src/android/HabitPlugin.java
@@ -114,7 +114,7 @@ public class HabitPlugin extends CordovaPlugin {
 
 		customization.setCustomScreens(customScreens);
 		  
-		DeviceHealth.performTests(cordova.getActivity().getApplicationContext(), cordova.getActivity(), appid, apikey, serialnumber, imei, testsToPerform2, customization, new TestCallback() {
+		DeviceHealth.performTests(cordova.getActivity().getApplicationContext(), cordova.getActivity(), appid, apikey, serialnumber != "" ? serialnumber : null, imei != "" ? imei : null, testsToPerform2, customization, new TestCallback() {
 		@Override
 		public void onResponse(JSONObject obj) {
 			if (obj != null) {              
